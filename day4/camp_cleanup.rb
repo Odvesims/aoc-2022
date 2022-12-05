125c4c8a0c6beb4ab4ced41388aeb8a0384abc09
Oscar Valdez <odvesims@gmail.com>
2022-12-05 17:03:01 -0400

    adding support for part 2

--- day4/camp_cleanup.rb
+++ day4/camp_cleanup.rb
@@ -2,19 +2,40 @@ require 'set'
 
 def overlapping_sections
   sections = File.open("assigned-sections.txt").read.split("\n")
+
+  #Part 1
   total_overlapping_sections = 0
+
   sections.each_with_index do |section, i|
-    part_one = Range.new(section.split(",")[0].split("-")[0], section.split(",")[0].split("-")[1])
-    part_two = Range.new(section.split(",")[1].split("-")[0], section.split(",")[1].split("-")[1])
-    set_one = Set.new(part_one)
-    set_two = Set.new(part_two)
+    sets = get_sets(section)
+    set_one = sets[0]
+    set_two = sets[1]
     if set_one >= set_two or set_two >= set_one
       total_overlapping_sections += 1
     end
   end
-  puts total_overlapping_sections
+  puts "Part 1: \n#{total_overlapping_sections}"
+
+  #Part 2
+  total_overlapping_sections = 0
+
+  sections.each do |section|
+    sets = get_sets(section)
+    set_one = sets[0]
+    set_two = sets[1]
+    if (set_one & set_two).count > 0
+      total_overlapping_sections += 1
+    end
+  end
+  puts "\nPart 2: \n#{total_overlapping_sections}"
 end
 
-#Puzzle's Part #1
+def get_sets(section)
+  part_one = Range.new(section.split(",")[0].split("-")[0], section.split(",")[0].split("-")[1])
+  part_two = Range.new(section.split(",")[1].split("-")[0], section.split(",")[1].split("-")[1])
+  [Set.new(part_one), Set.new(part_two) ]
+end
+
+#Day 4 Puzzles
 
 overlapping_sections
